864067b9d0001ad3b7f51e3aca9213ba9d38e297
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-04 10:46:25 +0300

    Initial Commit

---
+++ DetailedViews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DetailedViews
+//
+//  Created by Atil Samancioglu on 4.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
